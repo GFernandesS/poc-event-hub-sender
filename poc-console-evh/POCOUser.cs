20db67e178ce0bd0593c0ac817bebcf2d15f6382
Guilherme Fernandes Schiavone <guilherme.schiavone@aberto.com.br>
2021-09-25 10:25:45 -0300

    Implementation

---
+++ poc-console-evh/POCOUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace poc_console_evh
+{
+    public class POCOUser
+    {
+        private static List<string> possibleNames = new List<string> { "Guilherme", "Fernando", "Gustavo", "Felipe" };
+
+        public string Name { get; private set; } = possibleNames[new Random().Next(0, possibleNames.Count)];
+        public int Age { get; private set; } = new Random().Next(0, 50);
+    }
+}
---
+++ poc-console-evh/Program.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace poc_console_evh
+{
+    class Program
+    {
+        private static readonly string connectionString = "Endpoint=sb://evhns-poc-article.servicebus.windows.net/;SharedAccessKeyName=sas-access;SharedAccessKey=pM2vUmmsCIycmG47Bh8KTt7JE6BJ1DQF0DwM/758Ge4=;EntityPath=evh-poc-1";
+
+        private static readonly string topicName = "evh-poc-1";
+        static async Task Main()
+        {
+            await using var producerClient = new EventHubProducerClient(connectionString, topicName);
+
+            using (var eventBatch = await producerClient.CreateBatchAsync())
+            {
+                var events = GetEvents();
+
+                foreach (var eventData in events)
+                    eventBatch.TryAdd(eventData);
+
+                await producerClient.SendAsync(eventBatch);
+            }
+
+            WriteLine("Eventos enviados com sucesso");
+        }
+
+        static IEnumerable<EventData> GetEvents()
+        {
+            var users = new List<POCOUser> { new(), new() };
+
+            foreach(var user in users)
+                yield return new EventData(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(user)));
+        }
+    }
+}
